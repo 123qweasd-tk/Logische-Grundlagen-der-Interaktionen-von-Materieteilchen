25fad72922de243b3e498a1dcf1eaea44a956f2a
123qweasd-tk <71277656+123qweasd-tk@users.noreply.github.com>
2026-03-02 16:47:52 +0100

    Update print statement from 'Hello' to 'Goodbye'

--- main (1. Schritt) - MIT RICHTIGEN NAMEN - NOCH BESSER.py
+++ main (1. Schritt) - MIT RICHTIGEN NAMEN - NOCH BESSER.py
@@ -935,9 +935,11 @@ def deduction_of_tetradic_total_formulas_from_triadic_level( *args):
                     #for t, formula_4 in enumerate(list_syllogism_total_formulas):
                                                                                                                                             
             
-                first_formula_tri = formula_1[0]
-                third_formula_tri =  ['u', 'u', 'u', 'u', 'u', 'u', 'u', 'u']
+                first_formula_tri = ['u', 'u', 'u', 'u', 'u', 'u', 'u', 'u']
+
+                
                 second_formula_tri = formula_2[0]
+                third_formula_tri =  formula_1[0]
                 fourth_formula_tri = ['u', 'u', 'u', 'u', 'u', 'u', 'u', 'u']
                 #fourth_formula_tri = formula_4[2]
                 #print(first_formula_tri)
@@ -948,7 +950,7 @@ def deduction_of_tetradic_total_formulas_from_triadic_level( *args):
 
             
             
-                solution_and_contradiction_test = syllogism_contradiction_test_tet(first_formula_tri, second_formula_tri, third_formula_tri, fourth_formula_tri)
+                solution_and_contradiction_test = syllogism_contradiction_test_tet(third_formula_tri, second_formula_tri,  fourth_formula_tri, first_formula_tri)
                             #solution_and_contradiction_test[0].count('u') == 1 --> one 'u' in total-formula
                             #len(error_number) == 0 --> no contradiction
                                                                 
@@ -957,13 +959,13 @@ def deduction_of_tetradic_total_formulas_from_triadic_level( *args):
                             #    print(error_number)                        
                 #print(solution_and_contradiction_test)
                 if (solution_and_contradiction_test[0] != 'W'):
-                    first_formula = triadic_name_fn(first_formula_tri, 1)
+                    first_formula = triadic_name_fn(first_formula_tri, 4)
                     second_formula = triadic_name_fn(second_formula_tri, 2)
-                    third_formula = triadic_name_fn(third_formula_tri, 3)
-                    fourth_formula = triadic_name_fn(fourth_formula_tri, 4)
+                    third_formula = triadic_name_fn(third_formula_tri, 1)
+                    fourth_formula = triadic_name_fn(fourth_formula_tri, 3)
                                                                                                                                                         
 
-                    writer.writerow([solution_and_contradiction_test[0], [first_formula, second_formula, p, r]])
+                    writer.writerow([solution_and_contradiction_test[0], [second_formula], [third_formula], p, r])
                 
         file.close()    
                                                                                                                                           
